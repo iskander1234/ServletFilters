591dbdc5427d5c07350316234b03bc5306f660db
Iskander Dosgali <idosgali@gmail.com>
2021-02-19 23:59:43 +0600

    Добавил Account

--- src/main/java/com/example/ServletFilters/Account.java
+++ src/main/java/com/example/ServletFilters/Account.java
@@ -4,6 +4,7 @@ public class Account {
     private String username;
     private String password;
 
+
     public Account(String username, String password) {
         this.username = username;
         this.password = password;
