9ca028fbd6d323abddfe8b8c6628d36ae1dda7fa
Iskander Dosgali <idosgali@gmail.com>
2021-02-19 23:59:15 +0600

    Добавил AccountModel

--- src/main/java/com/example/ServletFilters/AccountModel.java
+++ src/main/java/com/example/ServletFilters/AccountModel.java
@@ -2,6 +2,7 @@ package com.example.ServletFilters;
 
 public class AccountModel {
     public boolean login(String username, String password){
+
         return username.equalsIgnoreCase("iska") && password.equalsIgnoreCase("123");
     }
 }
